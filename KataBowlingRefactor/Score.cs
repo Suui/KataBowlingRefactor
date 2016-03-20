f5a933d50d31edb5c3b6d77feec6629b011560a3
Suui <suui.gd@gmail.com>
2016-03-20 19:20:15 +0100

    Ready

---
+++ KataBowlingRefactor/Score.cs
@@ -0,0 +1,49 @@
+﻿namespace KataBowlingTwo
+{
+	public class Score
+	{
+		public static int For(string line)
+		{
+			var score = 0;
+			var previousScore = 0;
+			for (var i = 0; i < line.Length; i++)
+			{
+				if (line[i].Equals('X'))
+				{
+					var nextRoll = line[i + 1].Equals('-') ? 0
+								   : line[i + 1].Equals('X') ? 10
+									 : int.Parse(line[i + 1].ToString());
+
+					var secondNextRoll = line[i + 2].Equals('-') ? 0
+										 : line[i + 2].Equals('X') ? 10
+										   : line[i + 2].Equals('/') && line[i + 1].Equals('-') ? 10
+											 : line[i + 2].Equals('/') && !line[i + 1].Equals('-') ? 10 - int.Parse(line[i + 1].ToString())
+											   : int.Parse(line[i + 2].ToString());
+
+					score += 10 + nextRoll + secondNextRoll;
+					if (i + 2 == line.Length - 1) break;
+					previousScore = score;
+					continue;
+				}
+
+				if (line[i].Equals('/'))
+				{
+					var nextRoll = line[i + 1].Equals('-') ? 0
+								   : line[i + 1].Equals('X') ? 10
+									 : int.Parse(line[i + 1].ToString());
+
+					score = previousScore + 10 + nextRoll;
+					if (i + 1 == line.Length - 1) break;
+					previousScore = score;
+					continue;
+				}
+
+				previousScore = score;
+				if (line[i].Equals('-')) continue;
+				score += int.Parse(line[i].ToString());
+			}
+
+			return score;
+		}
+	}
+}
---
+++ KataBowlingRefactorTest/ScoreShould.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using KataBowlingTwo;
+using NUnit.Framework;
+
+
+namespace KataBowlingTwoTest
+{
+	[TestFixture]
+	class ScoreShould
+	{
+		[Test]
+		public void sum_single_rolls()
+		{
+			Score.For("--------------------").Should().Be(0);
+			Score.For("---1--1---11--1111--").Should().Be(8);
+		}
+
+		[Test]
+		public void sum_spare_rolls()
+		{
+			Score.For("-/------------------").Should().Be(10);
+			Score.For("-/1-----------------").Should().Be(12);
+			Score.For("1/1-----------------").Should().Be(12);
+		}
+
+		[Test]
+		public void sum_strike_rolls()
+		{
+			Score.For("X------------------").Should().Be(10);
+			Score.For("X-1----------------").Should().Be(12);
+			Score.For("X1-----------------").Should().Be(12);
+			Score.For("X11----------------").Should().Be(14);
+		}
+
+		[Test]
+		public void acceptance_test_with_only_normal_rolls()
+		{
+			Score.For("11111111111111111111").Should().Be(20);
+		}
+
+		[Test]
+		public void acceptance_test_without_spares_and_strikes()
+		{
+			Score.For("9-9-9-9-9-9-9-9-9-9-").Should().Be(90);
+		}
+
+		[Test]
+		public void acceptance_test_with_all_spares()
+		{
+			Score.For("5/5/5/5/5/5/5/5/5/5/5").Should().Be(150);
+		}
+
+		[Test]
+		public void acceptance_test_with_all_strikes()
+		{
+			Score.For("XXXXXXXXXXXX").Should().Be(300);
+		}
+
+		[Test]
+		public void acceptance_test_with_every_possible_roll()
+		{
+			Score.For("-/1-XX1/X-/-11-X-/").Should().Be(125);
+			Score.For("-/1-XX1/X-/-11-XX5").Should().Be(130);
+		}
+	}
+}
